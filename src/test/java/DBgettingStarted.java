80d89f5c086b8bace79c06f0df75be141e5eae3e
user2 <nika.jvelauri.1@btu.edu.ge>
2021-09-16 15:56:49 +0400

    done homework2

--- src/test/java/DBgettingStarted.java
+++ src/test/java/DBgettingStarted.java
@@ -1,42 +1,121 @@
-import com.codeborne.selenide.Configuration;
-import org.openqa.selenium.By;
-import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.sql.*;
-
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 
 public class DBgettingStarted {
 
-    @Test
-    public static  void fillInformation() throws ClassNotFoundException {
+    public static int insertCandidate(String ID, String Name,String surName, String phone) {
+        // for insert a new candidate
+
+
+        String insert = "INSERT INTO students(id,firstName,lastName,phone) "
+                + "VALUES(?,?,?,?)";
 
         String url = "jdbc:sqlserver://localhost:1433";
         String user = "user1";
         String password = "admin";
-        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");  //1. Register your driver
-        Configuration.startMaximized = true;
-        String urlOfWebPage = "https://demoqa.com/automation-practice-form";
 
+        String sqlUpdate = "UPDATE students "
+                + "SET firstName = ? "
+                + "WHERE id = ?";
+
+
+
+        int candidateId = 0;
         try {
             Connection conn = DriverManager.getConnection(url, user, password);
+            //Use autosaved false mode
+            conn.setAutoCommit(false);
             Statement stmt = conn.createStatement();
             String sql = "SELECT * FROM students";
             ResultSet rs = stmt.executeQuery(sql);
 
-            while (rs.next()) {
-                open(urlOfWebPage);
-                $(By.xpath("//*[@id='firstName']")).sendKeys(rs.getString("firstName"));
-                $(By.xpath("//*[@id='lastName']")).sendKeys(rs.getString("lastName"));
-                $(By.xpath("//*[@id='userNumber']")).sendKeys(rs.getString("phone"));
+            candidateId = 0;
+
+
+
+
+            PreparedStatement pstmt = conn.prepareStatement(insert,
+                    Statement.RETURN_GENERATED_KEYS);
+
+
+            // set parameters for statement
+
+            pstmt.setString(1, ID);
+            pstmt.setString(2, Name);
+            pstmt.setString(3, surName);
+            pstmt.setString(4, phone);
+
+            int rowAffected = pstmt.executeUpdate();
+            if (rowAffected == 1) {
+                System.out.println("Row has added SUCCSSEFULLY");
+                candidateId++;
+            }else {
+                System.out.println("row didnt add");
             }
+            conn.commit();
+
+
+
+
+
+            //Validate all the values of inserted row using TestNG
+            SoftAssert softAssert = new SoftAssert();
+            softAssert.assertEquals("1004",ID);
+            softAssert.assertEquals("Nika",Name);
+            softAssert.assertEquals("Jvelauri",surName);
+            softAssert.assertEquals("555667788",phone);
+            softAssert.assertAll();
+
+
+
+
+
+
+            PreparedStatement pstmt1 = conn.prepareStatement(sqlUpdate);
+
+            //Update firstName of added student
+            String firstName = "Name";
+            pstmt1.setString(1, firstName);
+            pstmt1.setString(2,"1004");
+            int rowAffected1 = pstmt1.executeUpdate();
+            //Validate updated firstName using TestNG
+            softAssert.assertEquals("Name",firstName);
+            softAssert.assertAll();
+            conn.commit();
+
 
 
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
+
+
+        return candidateId;
+
+
     }
 
+    public static void main(String[] args) {
+        // insert a new candidate
+        int id = insertCandidate("1004", "Nika", "Jvelauri", "555667788");
+
+        System.out.println("A new candidate with has been inserted");
+
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
